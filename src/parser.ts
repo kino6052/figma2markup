7dd27947b9671d9ead82d76cb8b08bec67c1d7b1
Kirill Novik <kino6052@colorado.edu>
2020-05-11 23:18:02 -0700

    Add List

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,30 @@
 // @ts-ignore
 const _ = global.__DEPENDENCIES__;
 
-const NodeSubject__parser = new _.Subject<[BaseNode, BaseNode]>('node') as Subject<[BaseNode, BaseNode]>;
-const OnLoadSubject__parser = new _.Subject<{}>('load') as Subject<{}>;
-const OnUnsubscribeSubject__parser = new _.Subject<{}>('unsubscribe') as Subject<{}>;
+const NodeSubject__parser = new _.Subject<[BaseNode, BaseNode]>(
+  "node"
+) as Subject<[BaseNode, BaseNode]>;
+const OnLoadSubject__parser = new _.Subject<{}>("load") as Subject<{}>;
+const OnUnsubscribeSubject__parser = new _.Subject<{}>(
+  "unsubscribe"
+) as Subject<{}>;
+
+let count = 0;
+
+interface IListItem {
+  id: string;
+  parentId: string;
+  before: string;
+  after: string;
+}
+
+class List {
+  list: Array<IListItem> = [];
+}
 
 NodeSubject__parser.subscribe((nodes) => {
-  console.warn("I have received", nodes)
+  count += 1;
+  console.warn(count, "I have received", nodes);
 });
 
 OnLoadSubject__parser.next({});
@@ -15,4 +33,4 @@ OnUnsubscribeSubject__parser.subscribe(() => {
   OnLoadSubject__parser.unsubscribe();
   NodeSubject__parser.unsubscribe();
   OnUnsubscribeSubject__parser.unsubscribe();
-})
+});
